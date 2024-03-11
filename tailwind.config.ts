613400d422fdaf1e41284a250228ef03344ad972
Maire Sangster <mairesangster@Maires-MacBook-Pro.local>
2024-03-11 14:16:34 -0600

    add custom colors to tailwind.config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,14 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      colors: {
+        primary: '#09063B', 
+        secondary: '#F6460B',
+        white: '#FFFFFF',
+        highlight: '#C5D8F8',
+        indigo: '#09063B', // intentionally duplicative for future proofing with theming
+        fire: '#F6460B' // intentionally duplicative for future proofing with theming
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
